6e01e2f797a37bd37a3b355fc594c5c1ef641aa0
saravana716 <vsaravana716@gmail.com>
2026-01-23 00:18:54 +0530

    yyyy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11', 'safari >= 11', 'ios >= 11'],
+    })
   ],
   resolve: {
     alias: {
@@ -15,7 +19,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    target: 'es2015',
     rollupOptions: {
       output: {
         manualChunks: {
