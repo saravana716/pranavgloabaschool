ab3fa728770a713e432ab135fd516ac0a672c007
saravana716 <vsaravana716@gmail.com>
2026-01-22 21:17:14 +0530

    fd

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import postcssPresetEnv from 'postcss-preset-env'
-import postcssCascadeLayers from '@csstools/postcss-cascade-layers'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
@@ -14,23 +12,10 @@ export default defineConfig({
       '@': resolve(__dirname, 'src')
     }
   },
-  css: {
-    postcss: {
-      plugins: [
-        postcssCascadeLayers(),
-        postcssPresetEnv({
-          stage: 3,
-          features: {
-            'oklab-function': { preserve: true }
-          }
-        })
-      ]
-    }
-  },
   build: {
     outDir: 'dist',
     sourcemap: false,
-    target: ['es2015', 'safari11'],
+    target: 'es2015',
     rollupOptions: {
       output: {
         manualChunks: {
