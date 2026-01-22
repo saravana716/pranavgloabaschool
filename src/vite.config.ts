f310c1365331202a6c08535b79f49f8181f54022
saravana716 <vsaravana716@gmail.com>
2026-01-22 20:11:31 +0530

    cghh

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11', 'safari >= 10', 'ios >= 10'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    })
   ],
   resolve: {
     alias: {
@@ -15,6 +20,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
+    target: ['es2015', 'edge88', 'firefox78', 'chrome87', 'safari13'],
     rollupOptions: {
       output: {
         manualChunks: {
